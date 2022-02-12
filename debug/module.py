b67e87a44dce0280d07ab52c48d8fcb3327c5521
ArcasCZ <VydraMan@seznam.cz>
2022-02-12 22:26:45 +0100

    Update module.py

--- debug/module.py
+++ debug/module.py
@@ -9,11 +9,8 @@ from PIL import Image, ImageFile
 import nextcord
 from nextcord.ext import commands, tasks
 
-from pie import utils, check
+from pie import utils, check, storage
 from pie.database import session
-
-MAX_ATTACHMENT_SIZE = 8000
-ALLOWED_FORMATS = ("jpg", "jpeg", "png", "webp", "gif")
 
 
 class Debug(commands.Cog):
@@ -24,12 +21,12 @@ class Debug(commands.Cog):
 
     @commands.guild_only()
     @commands.group(name="debug")
-    @commands.check(check.acl)
+    @check.acl2(check.ACLevel.BOT_OWNER)
     async def debug_(self, ctx):
         """Debug tools"""
         await utils.discord.send_help(ctx)
 
-    @commands.check(check.acl)
+    @check.acl2(check.ACLevel.BOT_OWNER)
     @debug_.command(name="emoji")
     async def debug_emoji(
         self, ctx, emoji: Optional[Union[nextcord.PartialEmoji, str]] = None
@@ -59,71 +56,32 @@ class Debug(commands.Cog):
 
         await ctx.send(embed=embed)
 
-    @commands.check(check.acl)
+    @check.acl2(check.ACLevel.BOT_OWNER)
     @debug_.command(name="str")
-    async def debug_str( self, ctx: commands.Context, *members: Union[nextcord.Member, int]):
+    async def debug_str(
+        self, ctx: commands.Context, *members: Union[nextcord.Member, int]
+    ):
         for member in members:
             if isinstance(member, int):
                 member_id = member
                 member = ctx.guild.get_member(member_id)
             else:
                 member_id = member.id
-                
-            print("{}, {}".format(member, member_id))
-            
 
-    @commands.check(check.acl)
+            print("{}, {}".format(member, member_id))
+
+    @check.acl2(check.ACLevel.BOT_OWNER)
     @debug_.command(name="message")
-    async def debug_message( self, ctx: commands.Context, message: nextcord.Message):
-        
-        """opener = urllib.request.build_opener()
-        opener.addheaders = [('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36')]
-        urllib.request.install_opener(opener)"""
         HEADERS = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36'}
-        
-        for url in re.findall(r'(https?://[^\s]+)', message.content):
-            print(url)
-            req = urllib.request.Request(url, headers=HEADERS)
-            file = urllib.request.urlopen(req, timeout=20)
-            size = file.headers.get("content-length")
-            if size: size = int(size)
-            
-            if not size or size > MAX_ATTACHMENT_SIZE * 1024:
-                print("Too fat")
-                continue
-                
-            type = file.headers.get("content-type").split("/")
-            
-            if len(type) != 2 or type[0] != "image" or type[1] not in ALLOWED_FORMATS:
-                print("Not allowed")
-                continue
-                
-            parser = ImageFile.Parser()
-            
-            downloaded = 0
-            
-            while downloaded <= MAX_ATTACHMENT_SIZE:
-                data = file.read(1024)
-                downloaded += 1
-                if not data:
-                    break
-                parser.feed(data)
-            file.close()
-            
-            try:
-                image = parser.close()
-            except OSError:
-                print("Error")
-                continue
-                
-            print(image)
-        print("Done")
-        
+    async def debug_message(self, ctx: commands.Context, message: nextcord.Message):
+        pass
+
+    @check.acl2(check.ACLevel.BOT_OWNER)
     @debug_.command(name="channels")
     async def debug_channels(self, ctx):
         channel_count = len(ctx.guild.channels)
         await ctx.send(f"There are {channel_count} channels.")
 
 
-def setup(bot) -> None:   
+def setup(bot) -> None:
     bot.add_cog(Debug(bot))
