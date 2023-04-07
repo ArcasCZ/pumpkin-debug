3d5284d49656586819b2eb55113c46e0ba9f7aca
ArcasCZ <VydraMan@seznam.cz>
2023-04-07 14:15:10 +0200

    Update module.py

--- vote/module.py
+++ vote/module.py
@@ -5,7 +5,7 @@ from discord.ext import commands
 
 from pie import utils, check
 
-class Debug(commands.Cog):
+class Vote(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
@@ -14,8 +14,23 @@ class Debug(commands.Cog):
     @commands.guild_only()
     @check.acl2(check.ACLevel.SUBMOD)
     @commands.command(name="vote")
-    async def vote(self, ctx, endTime: str, *, config: str):
-        await ctx.reply(config)
+    async def vote(self, ctx, datetime_str: str, *, config: str):
+        try:
+            end_time = utils.time.parse_datetime(datetime_str)
+        except dateutil.parser.ParserError:
+            await ctx.reply(
+                _(
+                    ctx,
+                    "I don't know how to parse `{datetime_str}`, please try again.",
+                ).format(datetime_str=datetime_str)
+            )
+            return
+        
+        await ctx.reply(end_time)
+           
+class VoteObject:
+    pass
+    
 
 async def setup(bot) -> None:
-    await bot.add_cog(Debug(bot))
+    await bot.add_cog(Vote(bot))
